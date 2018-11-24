d1f985f4bbcf77b1783ecccb04d80142c6825a2b
lerok <lerka-mishka@yandex.ru>
2018-11-24 14:05:20 +0300

    bug fix for four the latest tests for the functions get_actions

--- text_history.py
+++ text_history.py
@@ -2,7 +2,7 @@ class TextHistory:
     def __init__(self):
         self._text = ''
         self._version = 0
-        self._history = []
+        self._history = [] #мы поставили подчеркивани, потому что не хотим чтобы эта переменная могла быть вызавна вне класса
 
     @property
     def text(self):
@@ -26,32 +26,35 @@ class TextHistory:
 
     def check_pos(self, pos=None):
         if pos is None:
-            return
-
+            return len(self.text)
         if pos < 0 or pos >= len(self.text):
             raise ValueError('Недопустимое значение параметра pos')
+        return pos
 
     def check_length(self, pos, length):
-        pass
+        if pos + length > len(self.text):
+            raise ValueError('Недопустимое значение параметра length or pos')
+
 
     def insert(self, text, pos=None):
-        self.check_pos(pos)
+        pos = self.check_pos(pos)
         insert_action = InsertAction(pos=pos, text=text, from_version=self.version, to_version=self.version + 1)
         return self.action(insert_action)
 
     def replace(self, text, pos=None):
-        self.check_pos(pos)
+        pos = self.check_pos(pos)
         replace_action = ReplaceAction(pos=pos, text=text, from_version=self.version, to_version=self.version + 1)
         return self.action(replace_action)
 
     def delete(self, pos, length):
-        self.check_pos(pos)
+        pos = self.check_pos(pos)
+        self.check_length(pos, length)
         delete_action = DeleteAction(pos=pos, length=length, from_version=self.version, to_version=self.version + 1)
         return self.action(delete_action)
 
-    def double(self):
-        double_action = DoubleTextAction(pos=None, text=None, from_version=self.version, to_version=self.version + 1)
-        return self.action(double_action)
+    # def double(self):
+    #     double_action = DoubleTextAction(pos=None, text=None, from_version=self.version, to_version=self.version + 1)
+    #     return self.action(double_action)
 
     def action(self, action: 'Action'):
         self.check_version(action.from_version, action.to_version)
@@ -61,8 +64,18 @@ class TextHistory:
         self._history.append(action)
         return self.version
 
-    def get_actions(self, from_version=1, to_version=2):
-        pass
+    def get_actions(self, from_version=None, to_version=None):
+        length_history = len(self._history)
+        if from_version == to_version:
+            return []
+        if to_version is None:
+            to_version = length_history
+        if 0 <= from_version < to_version:
+            if to_version <= length_history:
+                return self._history[from_version:to_version]
+            else:
+                return self._history[from_version]
+        raise ValueError
 
 
 class Action:
@@ -101,13 +114,13 @@ class DeleteAction(Action):
         return new_text
 
 
-class DoubleTextAction(Action):
-    def apply(self, text: str):
-        return text * 2
+# class DoubleTextAction(Action):
+#     def apply(self, text: str):
+#         return text * 2
 
 
 if __name__ == '__main__':
     h = TextHistory()
-    # h.insert('abc')
-    h.double()
-    print(h.text)
+    # # h.insert('abc')
+    # h.double()
+    # print(h.text)
